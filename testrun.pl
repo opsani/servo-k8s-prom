dc11786015ac67628131b0119e7e6ca9abbfd399
Lorenzo Swank <lswank@gmail.com>
2020-07-01 05:13:57 +0900

    Replaced the syncronyous load gen with asyc

--- testrun.pl
+++ testrun.pl
@@ -1,12 +1,10 @@
 #!/usr/bin/perl
 
 use strict;
-use warnings;
 
 my $num = 10;
 my $num_port = 50;
 my $server_ip = "18.221.54.244";
-#my $server_ip = "localhost";
 my $cert_path = "./";
 
 if (@ARGV != 1) {
@@ -21,10 +19,9 @@ my $start_port = 5440;
 my $port = $start_port;
 my $end_port = $start_port + $num_port - 1;
 for (my $i = 0; $i < $num; $i++) {
-    system("curl -k https://$server_ip:$port/loadgenerator/exp$i/100");
+    system("nohup curl -k https://$server_ip:$port/loadgenerator/exp$i/2000 &");
     $port++;
     if ($port > $end_port) {
         $port = $start_port;
     }
 }
-exit 0;
